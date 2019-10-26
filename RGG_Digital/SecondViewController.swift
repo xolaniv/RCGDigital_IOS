3faf4e3929c40d54ef8142dd36f63bf95467d39e
Xolani <xvilakaz@student.wethinkcode.co.za>
2019-10-26 13:42:44 +0200

    Project creation

---
+++ RGG_Digital/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RGG_Digital
+//
+//  Created by Xolani VILAKAZI on 2019/10/26.
+//  Copyright © 2019 Xolani VILAKAZI. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
